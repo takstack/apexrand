08843a7a132dec7ae85568d1a5a282090af5f53d
takstack <randy@takstack.com>
2020-12-01 08:10:20 -0800

    added user platformt

--- db/user.go
+++ db/user.go
@@ -39,7 +39,7 @@ func Insuserfromfile() {
 	tx, err := db.Begin() //get connection
 	handleError(err)
 	//LOG.GL.Println("in batch insert after tx begin")
-	qry := "INSERT INTO user (eaddr, username,pass,sess_id,sess_exp,propername,teamassign,psnid,platform) VALUES(?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE username=?, pass=?, propername=?, psnid=?"
+	qry := "INSERT INTO user (eaddr, username,pass,sess_id,sess_exp,propername,teamassign,psnid,platform) VALUES(?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE username=?, pass=?, propername=?, psnid=?, platform=?"
 	//LOG.GL.Println("in batch insert after qry gotten", qry)
 	stmt, err := tx.Prepare(qry)
 	handleError(err)
@@ -47,8 +47,8 @@ func Insuserfromfile() {
 	for _, elem := range sl {
 		now := time.Now()
 		oldexp := now.AddDate(0, -1, 0)
-		log.Println("insuser elem[5]:", elem[5])
-		_, err = stmt.Exec(elem[3], elem[0], elem[1], "", oldexp, elem[2], 0, elem[4], elem[5], elem[0], elem[1], elem[2], elem[4])
+		//log.Println("insuser elem[5]:", elem[5])
+		_, err = stmt.Exec(elem[3], elem[0], elem[1], "", oldexp, elem[2], 0, elem[4], elem[5], elem[0], elem[1], elem[2], elem[4], elem[5])
 
 		if err != nil {
 			log.Println("DB Error on this row: ", elem)
