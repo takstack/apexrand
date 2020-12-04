2e5aa10dfba03d6b65584c6778d635f813783803
takstack <randy@takstack.com>
2020-12-03 19:23:04 -0800

    tshoot apigetgames

--- db/tourn.go
+++ db/tourn.go
@@ -156,7 +156,8 @@ func getplayersgames(player string) []Game {
 func apigetplayersgames(player string) []Game {
 	starttime := time.Date(2020, time.Month(12), 1, 0, 0, 0, 0, time.UTC)
 	endtime := time.Date(2011, time.Month(12), 5, 0, 0, 0, 0, time.UTC)
-	qry := "select gameid, username,totaldmg, handicap, adjdmg, tstamp from apigames where username=? and tstamp > '?' and tstamp < '?' order by totaldmg desc limit 15"
+
+	qry := "select gameid, username,totaldmg, handicap, adjdmg, tstamp from apigames where username=? and tstamp > ? and tstamp < ? order by totaldmg desc limit 15"
 	res, err := db.Query(qry, player, starttime, endtime)
 	handleError(err)
 	var sl []Game
