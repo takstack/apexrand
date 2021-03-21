8a0256b44d0db987b477b0da7c0e7c571c29c692
takstack <randy@takstack.com>
2021-03-21 13:04:00 -0700

    tshoot trackers

--- db/tourn.go
+++ db/tourn.go
@@ -250,8 +250,8 @@ func getplayersgamesspecdate(player string, tourndate time.Time) [][]string {
 	return sl
 } /////
 //Seltourngames will get game data
-func Seltourntrackers(p string) []Game {
-	u := Getproper(p)
+func Seltourntrackers(u string) []Game {
+	//u := Getproper(p)
 	gsl := apigetplayerstrackers(u)
 
 	//if there are no games, add empty set
